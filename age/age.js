e94bd7b3dcffee7d5da808a31a789ecfba6c158a
zohreh-z <zahedi.zohreh@gmail.com>
2012-07-24 08:03:16 +0430

    change.

--- age/age.js
+++ age/age.js
@@ -5,7 +5,7 @@ window.onload=function(){
 	function birthday(){
 		var now=new Date();
 		now1=new Date(now.getTime()-bday.getTime());
-		document.getElementById('txt').innerHTML=now1.getFullYear()-1970+' Year '+now1.getMonth()+' Month '+now1.getDay()+' Day '+now1.getHours()+' Hours '+now1.getMinutes()+' Minutes '+now1.getSeconds()+' Seconds ';;
+		document.getElementById('txt').innerHTML=now1.getFullYear()-1970+' Year '+now1.getMonth()+' Month '+(now1.getDay()-1)+' Day '+now1.getHours()+' Hours '+now1.getMinutes()+' Minutes '+now1.getSeconds()+' Seconds ';;
 	}
 	setInterval(birthday,50);
 
